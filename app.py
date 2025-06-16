f1df55d9cabc5d8583e4fef182df77f9d7c33132
mpou <mpmail@orange.fr>
2025-06-16 22:46:16 +0200

    ajout d'une database avec exercices et memory_state

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 # pylint: disable=missing-module-docstring
-import io
 
 import duckdb
-import pandas as pd
 import streamlit as st
 
 st.write(
@@ -12,70 +10,63 @@ Spaced Repetition System SQL practice
 """
 )
 
-CSV = """
-beverage, price
-orange juice, 2.5
-expresso, 2
-tea, 3
-"""
-beverages = pd.read_csv(io.StringIO(CSV))
 
-CSV2 = """
-food_item, food_price
-cookie, 2.5
-chocolatine, 2
-muffin, 3
-"""
-food_items = pd.read_csv(io.StringIO(CSV2))
 
 ANSWER_STR = """
 SELECT * FROM beverages
 CROSS JOIN food_items
 """
+con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
 
-solution_df = duckdb.sql(ANSWER_STR).df()
+#solution_df = duckdb.sql(ANSWER_STR).df()
+
+
 
 with st.sidebar:
-    option = st.selectbox(
+    theme = st.selectbox(
         "What would you like to review?",
-        ("Joins", "GroupBy", "WFunctions"),
+        ("cross_joins", "GroupBy", "WFunctions"),
         index=None,
         placeholder="Select a theme",
     )
+    st.write("You selected:", theme)
+
+    exercise = con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}'").df()
+    st.write(exercise)
 
 st.header("enter your code:")
 query = st.text_area(label="votre code SQL ici", key="user_input")
 
 
-if query:
-    result = duckdb.sql(query).df()
-    st.dataframe(result)
+#if query:
+#    result = duckdb.sql(query).df()
+#    st.dataframe(result)
 
 # if len(result.columns) != len(
 #    solution_df.columns
 # ):
 #    st.write("Some columns are missing")
 
-try:
-    result = result[solution_df.columns]
-    st.dataframe(result.compare(solution_df))
-except KeyError as e:
-    st.write("Some columns are missing")
+#try:
+ #   result = result[solution_df.columns]
+ #   st.dataframe(result.compare(solution_df))
+#except KeyError as e:
+#    st.write("Some columns are missing")
 
-n_lines_difference = result.shape[0] - solution_df.shape[0]
-if n_lines_difference != 0:
-    st.write(f"result has a {n_lines_difference} lines difference with the solution")
+#n_lines_difference = result.shape[0] - solution_df.shape[0]
+#if n_lines_difference != 0:
+#    st.write(f"result has a {n_lines_difference} lines difference with the solution")
 
 
-tab2, tab3 = st.tabs(["Tables", "Solution"])
+#tab2, tab3 = st.tabs(["Tables", "Solution"])
 
-with tab2:
-    st.write("table: beverages")
-    st.dataframe(beverages)
-    st.write("table: food_items")
-    st.dataframe(food_items)
-    st.write("expected:")
-    st.dataframe(solution_df)
+#with tab2:
+#    st.write("table: beverages")
+#    st.dataframe(beverages)
+#    st.write("table: food_items")
+#    st.dataframe(food_items)
+#    st.write("expected:")
+#    st.dataframe(solution_df)
 
-with tab3:
-    st.write(ANSWER_STR)
+#with tab3:
+#    st.write(ANSWER_STR)
